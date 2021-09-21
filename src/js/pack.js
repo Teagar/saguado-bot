1eac62889fad7bbf710414178d74e443752d3142
SrThanulk <cyanofc@gmail.com>
2021-09-21 17:45:47 -0300

    First commit

---
+++ src/js/pack.js
@@ -0,0 +1,34 @@
+const pack = (date, user, wame) => {
+return `┏━🔥࿗ SAGUADO-BOT SALT ࿗🔥━┓
+║                                                           
+║ _*🕐 𝐃𝐚𝐭𝐚 𝐞 𝐡𝐨𝐫𝐚: ${date} 🕐*_
+║ _*🙂 𝐔𝐬𝐮𝐚́𝐫𝐢𝐨: ${user} 🙂*_
+║ _*🌎 𝐖𝐚𝐦𝐞: ${wame} 🌎*_                                        
+║                                                           
+┣══════𝑺𝒊𝒈𝒂 𝒏𝒂𝒔 𝒓𝒆𝒅𝒆𝒔 𝒔𝒐𝒄𝒊𝒂𝒊𝒔══════┫
+║
+║ _*📷 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦:*_ @_teagar
+║
+║
+┗════════════════════════┛
+» 🥵𝙾𝚗𝚕𝚢𝚏𝚊𝚗𝚜 𝚎 𝚑𝚎𝚗𝚝𝚊𝚒 𝚗𝚘 𝚖𝚎𝚐𝚊🥵 «
+
+❁❧ *FOTOS DE HENTAI 720MB:* 
+_*🌐 https://mega.nz/folder/u1YD3ACK#gWqDXIAOp1nzXgEXJOiS5g 🌐*_
+
+❁❧ *PRINCESS STARLING(ONLY FANS)8.36GB:* 
+_*🌐https://mega.nz/folder/m0YHyK4Y#G8JPdIl6SKx5wZT1GfOKWw 🌐*_
+
+❁❧ *ELLES CLUB 28.68GB:* 
+_*🌐 https://mega.nz/folder/C5oCXDDD#rrdhJPt9C3d2a0fJHOfteg 🌐*_
+
+❁❧ *COCONUTKITTY(ONLY FANS)7.69GB:*
+_*🌐 https://mega.nz/folder/T55SyJII#Ppu7FlWm2iLSFSNjW_ky0Q 🌐*_
+
+❁❧ *Love Lilah:* 
+_*🌐 https://mega.nz/folder/jYwySARJ#J8z9xO8d_b73CmdyURfv4A 🌐*_
+
+❁❧ *Hidori:* 
+_*🌐 https://mega.nz/folder/utJTTaZI#eiLi9GJ2Xixe9jBljNsrIw🌐*_`
+}
+exports.pack = pack
